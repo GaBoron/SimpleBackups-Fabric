caed38c6263079210e257d1c1b50193f5bcc273a
MelanX <info@melanx.de>
2025-04-13 09:22:42 +0200

    Add config screen support

--- src/main/java/de/melanx/simplebackups/SimpleBackups.java
+++ src/main/java/de/melanx/simplebackups/SimpleBackups.java
@@ -10,6 +10,8 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
@@ -28,6 +30,7 @@ public class SimpleBackups {
         NeoForge.EVENT_BUS.register(new EventListener());
         modEventBus.addListener(this::setup);
         modEventBus.addListener(this::onRegisterPayloadHandler);
+        modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
 
         if (dist.isClient()) {
             modEventBus.register(new ClientEventHandler());
