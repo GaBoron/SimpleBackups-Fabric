3656917db13ae4d1e8d71c2a4110bc658851f5dd
MelanX <info@melanx.de>
2025-06-02 17:27:40 +0200

    Add config to save-all before backup

--- src/main/java/de/melanx/simplebackups/BackupThread.java
+++ src/main/java/de/melanx/simplebackups/BackupThread.java
@@ -210,6 +210,12 @@ public class BackupThread extends Thread {
     // vanilla copy with modifications
     private long makeWorldBackup() throws IOException {
         this.storageSource.checkLock();
+        if (CommonConfig.saveAll()) {
+            this.server.executeBlocking(() -> {
+                this.server.saveEverything(true, false, true);
+            });
+        }
+
         String fileName = this.storageSource.levelId + "_" + LocalDateTime.now().format(FORMATTER);
         Path path = CommonConfig.getOutputPath(this.storageSource.levelId);
 
